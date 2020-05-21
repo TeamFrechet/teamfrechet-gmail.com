58b9c8398e4adbf2b01bafd980ea29c6b87eac03
TeamFrechet <62988587+TeamFrechet@users.noreply.github.com>
2020-05-21 21:47:35 +0300

    21.5 kim
    main class for users

--- Inputfunction.py
+++ Inputfunction.py
@@ -3,17 +3,118 @@ import math
 import numpy as np
 import sympy as sym
 import frechetcalcu
+import curvesTrack as track
 
-def symbolFunction():#chose here the function
+# this class is the main class
+# use for input P,Q by user
+# this class also contain a function use for symbol curve to calculate frechet distance - not part of the final project
+# to use it approach main directly
+# use frechetcalcu to calculate the distance and curvesTrack to calculate and plot the path between the two curves
+
+# function draw
+# can use for all P matrix
+# do not forget to use plt.show() after using this function!
+def functionDraw(P,color): # P matrix 2 x n #color = tab:green
+    n = len(P)
+    for i in range(1,n):
+        plt.plot([P[i-1][0],P[i][0]],[P[i-1][1],P[i][1]],c=color)
+        print('draw')
+
+
+# symbol function O N L Y
+# useless for track calculate with points
+###############################
+def symbolFunction():
+
+    #chose here the function
     x = sym.symbols('x')
-    func1 = sym.sin(x) # P
-    func2 = sym.cos(x)# Q
+    func1 = x+2 # P
+    func2 = sym.sin(x)# Q
     return [func1,func2]
 
+
+# symbol function O N L Y
+# useless for track calculate with points
+###############################
+def matrixMaker(f,n,jump):
+    samples = []
+    x = sym.symbols('x')
+    for i in np.arange(0,n,jump):
+        samples.append([i,f.subs(x,i)])
+    print(samples)
+    return samples
+
+
+# research
+# symbol function O N L Y
+# useless for track calculate with points
+###############################
+def drawDistanceSamples(func1,func2,approx,jump):
+    P = matrixMaker(func1,approx,jump) # for taking samples - non polygonal curves
+    Q = matrixMaker(func2,approx,jump) # for taking samples - non polygonal curves
+    # P = [[1, 1], [2, 1], [2, 2]]
+    # Q = [[2, 2], [0, 1], [2, 4]]
+    frec = frechetcalcu.frechetDist(P,Q) # frec[0] is the leash length frec[1] is the distance matrix
+    print('leash size', frec[0]) # the leash size
+    X = ([P[i][0] for i in range(len(P))])
+    Py = ([P[j][1] for j in range(len(P))])
+    Qy = ([Q[j][1] for j in range(len(Q))])
+    plt.plot(X, Py, 'bo')
+    plt.plot(X,Py,'b')
+    plt.plot(X, Qy, 'bo')
+    plt.plot(X,Qy,'b')
+
+    plt.title(frec[0])
+    plt.suptitle('frechet distance=')
+    flagp = np.ones(len(P))
+    flagq = np.ones(len(P))
+    flagp = np.multiply(flagp,-1)
+    flagq = np.multiply(flagq,-1)
+
+    # give feasible path but not the best one
+
+    for i in range(len(frec[1])):
+        for j in range(len(frec[1])):
+            if ((frec[1])[i, j] <= frec[0] and flagp[i] == -1):
+                plt.plot([P[i][0], Q[j][0]], [P[i][1], Q[j][1]], c='tab:grey')
+                flagp[i] = j
+                flagq[j] = i
+    for i in range(len(frec[1])):
+        for j in range(len(frec[1])):
+            if (((frec[1]).transpose())[i,j] <= frec[0] and flagq[i] == -1 and flagp[i]!=j):
+                plt.plot([P[j][0], Q[i][0]], [P[j][1], Q[i][1]], c='tab:green')
+                flagq[i] = j
+    plt.show()
+
+
 if __name__ == '__main__':
-    print("main")
-    aproxximate = 5
-    frechetcalcu.Start(symbolFunction()[0],symbolFunction()[1],aproxximate)
+
+    # use this code if you want to use samples matrix
+
+    # aproxximate = 5
+    # samples_jump = 0.5
+    # P = symbolFunction()[0]
+    # Q = symbolFunction()[1]
+    # drawDistanceSamples(P,Q,aproxximate,samples_jump)
+
+    # use this code if you wat to use points
+
+    # draw the curves
+
+    P = [[0,0],[1,1],[2,2]]
+    Q = [[0,1],[1,2],[2,3]]
+    functionDraw(P,'tab:green')
+    functionDraw(Q,'tab:blue')
+
+    frec = frechetcalcu.frechetDist(P, Q)  # frec[0] is the leash lenght frec[1] is the distance matrix
+    plt.title(frec[0])
+    plt.suptitle('frechet distance=')
+    plt.figure()
+
+    ######## continue from here ########
+    # track.pathLimit(P,Q,0.1) # function in curvesTrack class for calculate the path
+    # this function is not finished yet, have some bugs
+
 
 
 
